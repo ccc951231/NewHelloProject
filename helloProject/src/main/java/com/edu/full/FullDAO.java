a04b8225a659bf43f911fc2e4b8b555425500ecb
juyeon <ccc951231@email.com>
2022-01-12 18:01:19 +0900

    220112

--- helloProject/src/main/java/com/edu/full/FullDAO.java
+++ helloProject/src/main/java/com/edu/full/FullDAO.java
@@ -1,4 +1,5 @@
 package com.edu.full;
+
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -7,34 +8,70 @@ import java.util.Map;
 
 import com.edu.common.DAO;
 
+
 // com.edu.full.FullDAO
 // title, startDate, endDate => VO, Map: key, value => 한건.
 public class FullDAO extends DAO {
+
+	public boolean deleteSchedule(Map<String, String> map) throws SQLException {
+		String sql = "delete from full_calendar where title=?";
+		connect();
+
+		psmt = conn.prepareStatement(sql);
+		psmt.setString(1, map.get("title"));
+
+		int r = psmt.executeUpdate();
+		if (r > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// 일정 등록하는 메소드.
+	public boolean insertSchedule(Map<String, String> map) throws SQLException {
+
+		String sql = "insert into full_calendar values(?,?,?)";
+		connect();
+		psmt = conn.prepareStatement(sql);
+		psmt.setString(1, map.get("title"));
+		psmt.setString(2, map.get("startDate"));
+		psmt.setString(3, map.get("endDate"));
+
+		int r = psmt.executeUpdate();
+		if (r > 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
 	// 전체 일정을 가지고 오는 메소드.
 	public List<Map<String, String>> getScedules() {
 		String sql = "select title, start_date, end_date from full_calendar";
-		connect(); //ctrl키 눌러서 DAO 연결을 확인할 수 있다.
+		connect(); // ctrl키 눌러서 DAO 연결을 확인할 수 있다.
 		// 반환타입 : List<Map<String, String>>
 		// [{title:"test", start:"2020-01-01", end:"2020-01-02"},{},{}]
-		List<Map<String, String>> scheduleList = new ArrayList<Map<String, String>>();	//배열에 Map타입의 인스턴스를 담을 수 있다, 왼쪽 오른쪽 타입은 같아야한다.
+		List<Map<String, String>> scheduleList = new ArrayList<Map<String, String>>(); // 배열에 Map타입의 인스턴스를 담을 수 있다, 왼쪽
+																						// 오른쪽 타입은 같아야한다.
 		try {
 			psmt = conn.prepareStatement(sql);
 			rs = psmt.executeQuery();
-			while(rs.next()) { // 건수만큼 반복한다.
+			while (rs.next()) { // 건수만큼 반복한다.
 				Map<String, String> map = new HashMap<String, String>();
 				map.put("title", rs.getString("title"));
 				map.put("start", rs.getString("start_date"));
-				map.put("end",  rs.getString("end_date"));
-				
+				map.put("end", rs.getString("end_date"));
+
 				scheduleList.add(map);
 			}
-		}catch(SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
-		}finally {
+		} finally {
 			disconnect();
 		}
-		
-		return scheduleList; 
+
+		return scheduleList;
 	}
 
 }
--- helloProject/src/main/java/com/edu/full/ScheduleServlet.java
+++ helloProject/src/main/java/com/edu/full/ScheduleServlet.java
@@ -1,6 +1,8 @@
 package com.edu.full;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,35 +15,103 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-
 @WebServlet("/ScheduleServlet")
 public class ScheduleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	
-    public ScheduleServlet() {
-        super();
-        
-    }
+	public ScheduleServlet() {
+		super();
 
-	
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		response.setContentType("text/json;charset=utf-8"); //json타입으로 사용
-		
-		FullDAO dao = new FullDAO(); //FullDAO를 호출한다(같은 패키지 내이므로 그냥 사용가능)
+		// 조회할때 사용. doGet
+		response.setContentType("text/json;charset=utf-8"); // json타입으로 사용
+
+		FullDAO dao = new FullDAO(); // FullDAO를 호출한다(같은 패키지 내이므로 그냥 사용가능)
 		List<Map<String, String>> schedules = dao.getScedules();
-		
+
 		Gson gson = new GsonBuilder().create();
-		response.getWriter().print(gson.toJson(schedules)); //웹 브라우저에 출력.
-		
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.getWriter().print(gson.toJson(schedules)); // 웹 브라우저에 출력.
+
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-	
-		doGet(request, response);
-	}
 
+		response.setContentType("text/json;charset=utf-8");
+		// 스케줄 등록하는 서블릿.
+		request.setCharacterEncoding("utf-8");
+
+		
+		String action = request.getParameter("action");
+		
+		//입력
+		if (action.equals("insert")) {
+
+			String title = request.getParameter("title");
+			String start = request.getParameter("start");
+			String end = request.getParameter("end");
+
+			// Map 타입에 사용자 입력값을 전부 담아두기.
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("title", title);
+			map.put("startDate", start);
+			map.put("endDate", end);
+
+			FullDAO dao = new FullDAO();
+			String result = "";
+
+			try {
+				if (dao.insertSchedule(map)) {
+					// {"retCode":"Sucess"}
+					result = "{\"retCode\":\"Sucess\"}";
+				} else {
+					result = "{\"retCode\":\"Fail\"}";
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+				result = "{\"retCode\":\"Fail\"}";
+			}
+			// 웹브라우저로 json형태의 값을 송신.
+			response.getWriter().print(result);
+
+			// 입력처리
+		} else if (action.equals("delete")) {
+			
+			
+			// 삭제처리
+
+				String title = request.getParameter("title");
+				String start = request.getParameter("start");
+				String end = request.getParameter("end");
+
+				// Map 타입에 사용자 입력값을 전부 담아두기.
+				Map<String, String> map = new HashMap<String, String>();
+				map.put("title", title);
+				map.put("startDate", start);
+				map.put("endDate", end);
+
+				FullDAO dao = new FullDAO();
+				String result = "";
+
+				try {
+					if (dao.deleteSchedule(map)) {
+						// {"retCode":"Sucess"}
+						result = "{\"retCode\":\"Sucess\"}";
+					} else {
+						result = "{\"retCode\":\"Fail\"}";
+					}
+				} catch (SQLException e) {
+					e.printStackTrace();
+					result = "{\"retCode\":\"Fail\"}";
+				}
+				// 웹브라우저로 json형태의 값을 송신.
+				response.getWriter().print(result);
+
+				// 입력처리
+					
+		}
+	}
 }
